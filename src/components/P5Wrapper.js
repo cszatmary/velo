fbbc0069d85160e2ad0db32d0739259bcdfea51c
Christopher Szatmary <christopherszatmary@gmail.com>
2018-11-04 10:20:18 -0500

    Added ability to pass props from the P5Wrapper to the p5 sketch, added helper functions to center the sketch, read the data, and track acceleration

--- src/components/P5Wrapper.js
+++ src/components/P5Wrapper.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import p5 from 'p5';
 
 import sketch from '../p5';
 
-export default class P5Wrapper extends Component {
+class P5Wrapper extends Component {
 
   componentDidMount() {
     this.canvas = new p5(sketch, this.refs.wrapper);
   }
 
   componentWillReceiveProps(newProps) {
-
+    this.canvas.pushProps(newProps.p5);
   }
 
   render() {
-    return <div ref="wrapper" />
+    return <div id="p5-wrapper" ref="wrapper" />
   }
 }
+
+function mapStateToProps({ p5 }) {
+  return { p5 };
+}
+
+export default connect(mapStateToProps)(P5Wrapper);
--- src/p5/index.js
+++ src/p5/index.js
@@ -1,28 +1,76 @@
 import SerialPort from './SerialPort';
+import Ball from './Ball';
+import ballImg from "../assets/velo_ball.png";
 
 const DEFAULT_SERIAL_PORT = '/dev/tty.ATLAS7-ESP32_SPP_SERVER';
 
 export default p => {
   let serial;
+  const ball = new Ball();
+
+  let props = {};
 
   p.setup = function() {
     p.createCanvas(p.windowWidth, p.windowHeight);
-
+    p.loadImage(ballImg, img => ball.setImg(img));
     serial = new SerialPort();
 
     serial.open(DEFAULT_SERIAL_PORT, () => {
       console.log('Serial port is open');
     });
+
+    // serial.list(list => list.forEach(el => console.log(el)));
+  };
+
+  p.windowResized = () => {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
   };
 
   p.draw = () => {
-    p.background(100);
-    p.fill('#fff');
-    if (serial.available() > 0) {
-      var data = serial.read();
-      p.ellipse(50, 50, data, data);
+    centerOrigin();
+    p.background('#fff');
+    if (props.isGridVisible) {
+      drawGrid();
     }
+    p.fill('000000');
+    trackAcceleration(readData());
+    ball.draw(p);
   };
+
+  p.pushProps = (newProps) => {
+    props = newProps;
+    console.log(props);
+  };
+
+  function drawGrid() {
+    p.stroke(200);
+    p.fill(100);
+    for (let x = -p.width; x < p.width; x += 40) {
+      p.line(x, -p.height, x, p.height);
+      p.text(x, x + 1, 12);
+    }
+    for (let y = -p.height; y < p.height; y += 40) {
+      p.line(-p.width, y, p.width, y);
+      p.text(y, 1, y + 12);
+    }
+  }
+
+  function centerOrigin() {
+    p.translate(p.width / 2, p.height / 2);
+    p.scale(3.5);
+  }
+
+  function readData() {
+    return serial.available() > 0 ? serial.readLine() : undefined;
+  }
+
+  function trackAcceleration(data) {
+    if (!data) {
+      return;
+    }
+
+    ball.addAcceleration(JSON.parse(data));
+  }
 
   return p;
 };
